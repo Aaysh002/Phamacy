3815ab96d942ddb80d52408a59413fe3eb3238cb
Aaysh002 <159047931+Aaysh002@users.noreply.github.com>
2025-08-22 17:26:40 +0530

    Add files via upload

---
+++ pms final/pha/src/dao/PharmacyUtils.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author Hp user
+ */
+
+public class PharmacyUtils {
+   
+    public static String billPath = "E:\\Bills\\"; 
+    
+}
+
+
+    
+
